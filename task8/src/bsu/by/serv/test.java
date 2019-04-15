c069b6255943348c2b02e610fb02a2fdb49d3eab
Ilya <lyah.ilya88@gmail.com>
2019-04-15 20:34:39 +0300

    added task8

---
+++ task8/src/bsu/by/serv/test.java
@@ -0,0 +1,15 @@
+package bsu.by.serv;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().println("<html><h2 style = 'color:red'>Application Is Running</h2></html>");
+    }
+
+}
---
+++ task8/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User-PC
+  Date: 15.04.2019
+  Time: 14:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+  Java Web Application
+  </body>
+</html>
